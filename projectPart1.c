dd121637ebc72c750f364ad8c86f95eb71fcce30
Rejoan Ahmed <rejoanahmed8@gmail.com>
2023-04-12 20:32:03 +0800

    remove declaration

--- projectPart1.c
+++ projectPart1.c
@@ -1,26 +1,3 @@
-/**
- * ENGG1110 Problem Solving by Programming
- *
- * Course Project
- *
- * I declare that the project here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * University Guideline on Academic Honesty:
- *   https://www.cuhk.edu.hk/policy/academichonesty/
- *
- * Student Name  : Md Rejoan Ahmed
- * Student ID    : 1155187511
- * Class/Section : B
- * Date          : 2022/11/13
- */
-
 #include <stdio.h>
 /* NO other header files are allowed */
 /* NO global variables are allowed */
